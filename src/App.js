bffc50157b558d7913a6a9346c137fd45ddab482
pollina iliev <pilaeev@hotmail.com>
2021-05-13 22:51:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+  <div className="App">
+    <h1>Weather App</h1>
+    <footer>
+     Coded by  Pollina Iliev {" "} 
+      <a 
+      href="https://github.com/pilaee/react-weather-app"
+      target="_blank"
+      rel="noreferrer"
+      >
+        open-sourced on GitHub
+      </a>
+    </footer>
+    </div>
+  );
 }
 
-export default App;
+
