a77dac22c0519c2c82caf1d994e56187a0fcdbef
pollina iliev <pilaeev@hotmail.com>
2021-05-14 18:29:59 +0200

    update files

---
+++ src/Weather.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather (){
+    return(
+         <div className="Weather">
+            <h1> Berlin </h1>
+</div>
+    );
+}
