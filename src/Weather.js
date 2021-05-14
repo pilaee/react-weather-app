7859141ff2e2e9da2d261208eb0a6079182ee1ac
pollina iliev <pilaeev@hotmail.com>
2021-05-14 21:35:39 +0200

    update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,42 @@ import "./Weather.css";
 export default function Weather (){
     return(
          <div className="Weather">
+             <form>
+            <div className="row">
+            <div className="col-9">
+                 <input 
+                 type="search" 
+                 placeholder="Enter a city..."
+                 className="form-control"
+                 />
+               </div>
+               <div className="col-3">
+                 <input type="submit" value="Search" className="btn
+                 btn-primary" />
+                </div>
+            </div>
+             </form>
             <h1> Berlin </h1>
-</div>
-    );
+            <ul>
+                <li>Wednesday 07:00</li>
+                <li>Mostly Cloudy</li>
+            </ul>
+            <div className="row">
+                <div className="col-6">
+                <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="Mostly Cloudy"
+                />
+                6
+            </div>
+            <div className="col-6">
+                <ul>
+                    <li>Precipitation:15%</li>
+                    <li>Humidity:72%</li>
+                    <li>Wind:13 km/h% </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+ );
 }
