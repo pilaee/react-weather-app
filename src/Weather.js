22a63cdb00e85bb842723eccfea404a53fc877de
pollina iliev <pilaeev@hotmail.com>
2021-05-15 09:41:48 +0200

    install axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,45 +1,73 @@
 import React from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather (){
+export default function Weather (props){
+    const [weatherData,setWeatherData]=useState({ready:false});
+    const [city,setCity]=useState(props.defaultCity);
+
+
+function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey ="78e84724dd24f5c8d15376301785551c"
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+}
+
+
+
+if (weatherData.ready){
     return(
          <div className="Weather">
-             <form>
+             <form onSubmit={handleSubmit}>
             <div className="row">
             <div className="col-9">
                  <input 
                  type="search" 
                  placeholder="Enter a city..."
                  className="form-control"
+                 autoFocus="on"
+                 onChange={handleCityChange}
                  />
                </div>
                <div className="col-3">
-                 <input type="submit" value="Search" className="btn
-                 btn-primary" />
+                 <input type="submit" 
+                 value="Search" 
+                 className="btn btn-primary w-100" />
                 </div>
             </div>
              </form>
-            <h1> Berlin </h1>
-            <ul>
-                <li>Wednesday 07:00</li>
-                <li>Mostly Cloudy</li>
-            </ul>
-            <div className="row">
-                <div className="col-6">
-                <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="Mostly Cloudy"
-                />
-                6
-            </div>
-            <div className="col-6">
-                <ul>
-                    <li>Precipitation:15%</li>
-                    <li>Humidity:72%</li>
-                    <li>Wind:13 km/h% </li>
-                </ul>
-            </div>
-        </div>
+            <WeatherInfo data={weatherData} />
+            <WeatherForecast coordinates={weatherData.coordinates} />
+            
     </div>
  );
+} else {
+    search();
+    return"Loading...";
+ }
 }
+
+
